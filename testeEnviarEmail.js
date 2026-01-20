ad4ccde4420bb1c0930f5d39701b9be9be69b41e
Humberto Monteiro <humbertoaraujox@icloud.com>
2026-01-20 06:58:29 -0300

    att

--- testeEnviarEmail.js
+++ testeEnviarEmail.js
@@ -2,7 +2,7 @@ const emailService = require("./src/services/EmailService");
 
 const templateData = {
   templateId: "eventMessageSale",
-  status: "test",
+  status: "approved",
 };
 
 emailService
