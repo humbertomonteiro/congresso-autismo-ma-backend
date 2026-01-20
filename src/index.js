9f28caaeb449850c6a469c6ad3507eb884ac2417
Humberto Monteiro <humbertoaraujox@icloud.com>
2026-01-20 07:31:44 -0300

    ajuste no nome .platform

--- src/index.js
+++ src/index.js
@@ -26,19 +26,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(
-  cors({
-    origin: [
-      "https://congressoautismoma.com.br",
-      "https://www.congressoautismoma.com.br",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
 app.use(responseMiddleware);
 app.use(express.urlencoded({ extended: true }));
